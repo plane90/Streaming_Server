08e5d849cde5d80ffd83bbd729da50fa3b9b2e4a
plane90 <plane90@naver.com>
2021-05-26 19:02:05 +0900

    Update test.js
    'recorded' middleware created

--- streaming_app/routes/test.js
+++ streaming_app/routes/test.js
@@ -149,7 +149,40 @@ router.get('/multi_view/:screenCnt', (request, response) => {
         }
         </script>`;
     html = template.multiViewWithoutThree(videoNodes, script);
-    response.set({'access-control-allow-origin': '*'});
+    response.set({
+        'access-control-allow-origin': '*'
+    });
+    response.send(html);
+});
+
+router.get('/recorded/', (request, response) => {
+    var videoNode = `
+    <div id=recorded_play>
+    <video id="video" loop muted crossOrigin="anonymous" playsinline>
+        <source id="videoSrc" src="/recorded/2021-05-12 14-37-58.m3u8" type="application/x-mpegURL">
+    </video>
+    </div>`;
+    var script = `
+        <script type="module">
+        var hls = new Hls();
+        var video = document.getElementById('video');
+        hls.loadSource(document.getElementById('videoSrc').getAttribute('src'));
+        hls.attachMedia(video);
+        video.play();
+        video.setAttribute('height', window.innerHeight);
+        video.setAttribute('width', window.innerWidth);
+
+        window.addEventListener( 'resize', onWindowResize );
+        function onWindowResize() {
+            video.setAttribute('height', window.innerHeight);
+            video.setAttribute('width', window.innerWidth);
+        }
+
+        </script>`;
+    html = template.multiViewWithoutThree(videoNode, script);
+    response.set({
+        'access-control-allow-origin': '*'
+    });
     response.send(html);
 });
 
